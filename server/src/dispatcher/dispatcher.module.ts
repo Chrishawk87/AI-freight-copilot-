f613999b8146c2aebaeb46d5718b6e0a995dc8f9
Chris Hawkins <info@originmanagementsolutions.com>
2026-07-18 14:33:55 -0500

    Fix DI: import LearningModule into DispatcherModule (LoadsService dep)

--- server/src/dispatcher/dispatcher.module.ts
+++ server/src/dispatcher/dispatcher.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DispatcherController } from './dispatcher.controller';
 import { LoadsService } from '../loads/loads.service';
+import { LearningModule } from '../learning/learning.module';
 
 @Module({
+  imports: [LearningModule],
   providers: [LoadsService],
   controllers: [DispatcherController],
 })
